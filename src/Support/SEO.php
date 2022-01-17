f12f23f530e8e392717b98ba1521c246dd9111fc
Oshane Bailey <b4.oshany@gmail.com>
2022-01-17 06:05:43 +0000

    close: improve the SEO trait

--- src/Support/SEO.php
+++ src/Support/SEO.php
@@ -2,13 +2,43 @@
 
 namespace  Osoobe\LaravelTraits\Support;
 
+use Osoobe\Utilities\Helpers\Str;
 
 trait SEO {
 
-    public abstract function getRouteURL();
-    public abstract function getSEOTitleAttribute();
-    public abstract function getSEODescriptionAttribute();
+    public $seo_model_name;
 
+    /**
+     * SEO Route url
+     *
+     * @return string
+     */
+    public abstract function getRouteURL();
+
+    /**
+     * SEO title
+     *
+     * @return string
+     */
+    public abstract function getSEOTitleAttribute();
+    
+    /**
+     * SEO description
+     *
+     * @return string
+     */
+    public function getSEODescriptionAttribute() {
+        $subject = array_pop(explode("\\", get_class($this)));
+        $subject = Str::ucsnake(Str::pluralStudly($subject), " ");
+        $app_name = config('app.name');
+        return "Check out $this->seo_title and other $subject on $app_name";
+    }
+
+    /**
+     * URL
+     *
+     * @return string
+     */
     public function getURLAttribute(){
         return $this->getRouteURL();
     }
