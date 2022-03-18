6d123e8469b2d720546c2c0d87f7eb5e7ef21f74
Oshane Bailey <b4.oshany@gmail.com>
2022-03-18 11:01:13 +0000

    close: add toString trait

---
+++ src/Support/ToString.php
@@ -0,0 +1,66 @@
+<?php
+
+namespace  Osoobe\LaravelTraits\Support;
+
+use Illuminate\Support\Str;
+use Osoobe\Utilities\Helpers\FormatHelper;
+
+trait ToString {
+
+
+    /**
+     * To string method
+     *
+     * @param string $delimiter
+     * @param string $format
+     * @param string ...$strings
+     * @return string
+     */
+    public function toString(string $delimiter="\n", string $format=null, ...$string) {
+        return $this->propertyToString($delimiter, $format, ...$string);
+    }
+
+    /**
+     * Execute multiple toString for model attributes
+     *
+     * @param string $delimiter
+     * @param string $format
+     * @param string ...$strings
+     * @return string
+     */
+    public function propertyToString(string $delimiter="\n", string $format=null, string ...$strings): string {
+        $string = "";
+        $func = function ($value) use($delimiter, $format) {
+            if ( is_string($value) ) {
+                return FormatHelper::formatString($value, $format, Str::headline($value))
+                    .$delimiter;
+            }elseif ( method_exists($value, 'toString') ) {
+                try {
+                    return $value->toString($delimiter, $format);
+                } catch (\Throwable $th) {
+                    logger($th->getMessage());
+                }
+            }
+            return "";
+        };
+
+        foreach($strings as $property) {
+            if ( property_exists($this, $property)) {
+                $string .= $func($this->property);
+            } elseif ( method_exists($this, $property) ) {
+                $string .= $this->$property($delimiter, $format);
+            } else {
+                try {
+                    $string .= $func($this->$property);
+                } catch (\Throwable $th) {
+                    logger($th->getMessage());
+                }
+            }
+        }
+
+        return $string;
+    }
+
+}
+
+?>
